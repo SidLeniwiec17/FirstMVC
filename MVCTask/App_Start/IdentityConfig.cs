544a0c6c359db750a63d0748485d1c91463f6956
SidLeniwiec <piotrmichalik17@wp.pl>
2017-03-06 13:16:17 +0100

    wypróta apka na czysto prawie

--- MVCTask/App_Start/IdentityConfig.cs
+++ MVCTask/App_Start/IdentityConfig.cs
@@ -33,7 +33,7 @@ namespace MVCTask
     }
 
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-    public class ApplicationUserManager : UserManager<ApplicationUser>
+    /*public class ApplicationUserManager : UserManager<ApplicationUser>
     {
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
@@ -105,5 +105,5 @@ namespace MVCTask
         {
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
-    }
+    }*/
 }
