ee6ec9a7a5eec51d5c2ce6b5d37e6cd06bb55e71
SidLeniwiec <piotrmichalik17@wp.pl>
2017-03-06 13:04:07 +0100

    first commit on MVC

---
+++ MVCTask/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTask.Models;
+
+namespace MVCTask.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer//Init
+        public ActionResult Init()
+        {
+            //guzik
+            //open file dialog
+            //odpal inna akcje
+            return View();
+        }
+        // GET: Customer//Show
+        public ActionResult Show()
+        {
+            //wczytuje xmla
+            //tworze liste zgodnie z modelem
+            //zwracam do view
+            return View();
+        }
+    }
+}
---
+++ MVCTask/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTask.Models
+{
+    public class Customer
+    {
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public int number { get; set; }
+        public string email { get; set; }
+    }
+}
